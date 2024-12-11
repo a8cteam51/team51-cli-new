8e24616182728b5e2935a4a74bb45cb79fbfbf4a
Nick Green <nick.green@automattic.com>
2024-12-11 10:58:11 -0800

    Dealing with sites that have new JP ID

--- commands/Jetpack_Site_Connection_Triage.php
+++ commands/Jetpack_Site_Connection_Triage.php
@@ -14,19 +14,8 @@ use WPCOMSpecialProjects\CLI\Helper\AutocompleteTrait;
  * Triages the connection status of a given list of sites
  */
 #[AsCommand( name: 'jetpack:connection-triage' )]
-final class Jetpack_Site_Connection_triage extends Command {
+final class Jetpack_Site_Connection_Triage extends Command {
 	use AutocompleteTrait;
-
-	// region FIELDS AND CONSTANTS
-
-	/**
-	 * WPCOM site definition to fetch the information for.
-	 *
-	 * @var \stdClass|null
-	 */
-	private ?\stdClass $site = null;
-
-	// endregion
 
 	// region INHERITED METHODS
 
@@ -44,10 +33,10 @@ final class Jetpack_Site_Connection_triage extends Command {
 	 * {@inheritDoc}
 	 */
 	protected function execute( InputInterface $input, OutputInterface $output ): int {
-		$output->writeln( "<fg=magenta;options=bold>Listing Jetpack modules information for {$this->site->name} (ID {$this->site->ID}, URL {$this->site->URL}).</>" );
-
 		// Read in the CSV
 		$csv_path = $input->getArgument( 'csv-path' );
+
+		$output->writeln( "<fg=magenta;options=bold>Reading sites from {$csv_path}...</>" );
 
 		// For each row in the CSV, check the connection status
 		if ( ! file_exists( $csv_path ) ) {
@@ -63,24 +52,26 @@ final class Jetpack_Site_Connection_triage extends Command {
 				$line             = str_getcsv( $row );
 				$site_id          = $line[0];
 				$site_url         = $line[1];
+				$domain           = parse_url( $site_url, PHP_URL_HOST );
 				$jetpack_endpoint = $site_url . '/wp-json/jetpack/v4';
 				$notes            = '';
-				$status_code	  = '';
+				$status_code      = '';
+				$new_blog_id      = '';
 
-				$output->writeln( 'Checking ' . $site_url . '...');
+				$output->writeln( 'Checking ' . $site_url . '...' );
 
 				//fetch
-				$options = array(
+				$options     = array(
 					'http' => array(
-	
+
 						'method'        => 'GET',
 						'timeout'       => 60,
 						'ignore_errors' => true,
-					)
+					),
 				);
-				$context = stream_context_create( $options );
-				$result = @file_get_contents( $site_url, false, $context );
-				$headers = parse_http_headers( $http_response_header );
+				$context     = stream_context_create( $options );
+				$result      = @file_get_contents( $site_url, false, $context );
+				$headers     = parse_http_headers( $http_response_header );
 				$status_code = $headers['http_code'];
 
 				if ( 410 === $status_code && strpos( $site_url, 'mystagingwebsite.com' ) !== false ) {
@@ -90,32 +81,53 @@ final class Jetpack_Site_Connection_triage extends Command {
 				} elseif ( 404 === $status_code && strpos( $site_url, 'mystagingwebsite.com' ) !== false ) {
 					// probably deleted or Jetpack not installed. Next step is to curl the homepage
 					//$notes = $result['body'];
-					$notes = "404, check site";
+					$notes = '404, check site';
 					if ( strpos( $result, 'Domain not found' ) !== false ) {
 						$notes = 'Domain not found, probably deleted from Pressable';
 					}
 				} elseif ( 500 === $status_code ) {
 					//$notes = $result['body'];
-					$notes = "Check site, internal server error";
-				} elseif ( 200 === $status_code && strpos( $site_url, 'mystagingwebsite.com' ) == false ) {
-					$notes = 'Site either moved hosts or has a new JP connection. Check DARC and NA: https://mc.a8c.com/tools/reportcard/domain/?domain=' . $site_url . ' and https://wordpress.com/wp-admin/network/sites.php?s=' . $site_url;
+					$notes = 'Check site, internal server error';
+				} elseif ( 200 === $status_code && strpos( $site_url, 'mystagingwebsite.com' ) === false ) {
+					$notes = 'Site either moved hosts or has a new JP connection. Check DARC and NA: https://mc.a8c.com/tools/reportcard/domain/?domain=' . $domain . ' and https://wordpress.com/wp-admin/network/sites.php?s=' . $site_url;
+				} elseif ( 200 === $status_code && strpos( $site_url, 'mystagingwebsite.com' ) !== false ) {
+					// Check WPCOM API for site information
+					$wpcom_api_url = 'https://public-api.wordpress.com/rest/v1.1/sites/' . rawurlencode( $domain );
+					$wpcom_result  = @file_get_contents( $wpcom_api_url, false, $context );
+					$wpcom_status  = $http_response_header ? parse_http_headers( $http_response_header )['http_code'] : '';
+
+					if ( '400' === $wpcom_status ) {
+						$notes = 'Site is not connected to WordPress.com. If site loads, it may have moved hosts.';
+					} else {
+						$wpcom_data = json_decode( $wpcom_result, true );
+
+						if ( $wpcom_data && isset( $wpcom_data['ID'] ) ) {
+							if ( (int) $wpcom_data['ID'] !== (int) $site_id ) {
+								$notes       = 'Site has a new Jetpack connection. Old ID: ' . $site_id . ', New ID: ' . $wpcom_data['ID'];
+								$new_blog_id = $wpcom_data['ID'];
+							} else {
+								$notes = 'Site connection appears valid. Original error may have been intermittent.';
+							}
+						}
+					}
 				}
 				// wp-json/jetpack/v4/connection/data
 
 				$broken_site_data[] = array(
-					'site_id'  => $site_id,
-					'site_url' => $site_url,
-					'status'   => $status_code,
-					'notes'    => $notes,
+					'site_id'     => $site_id,
+					'site_url'    => $site_url,
+					'status'      => $status_code,
+					'new_blog_id' => $new_blog_id,
+					'notes'       => $notes,
 				);
 
 				//var_dump( $broken_site_data );
 			}
 			// Write a new CSV from the array
 			$output->writeln( '<info>Making the CSV...<info>' );
-			$timestamp = date( 'Y-m-d-H-i-s' );
+			$timestamp = gmdate( 'Y-m-d-H-i-s' );
 			$fp        = fopen( 'jp-connection-' . $timestamp . '.csv', 'w' );
-			fputcsv( $fp, array( 'Site ID', 'Site URL', 'Status', 'Notes' ) );
+			fputcsv( $fp, array( 'Site ID', 'Site URL', 'Status', 'New Blog ID', 'Notes' ) );
 			foreach ( $broken_site_data as $fields ) {
 				fputcsv( $fp, $fields );
 			}
