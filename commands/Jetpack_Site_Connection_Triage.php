a49e0492d3bcf4bccf7521e501e4c53565bd3bca
Nick Green <nick.green@automattic.com>
2025-01-22 11:11:18 -0800

    Add staging site check, PHPCS formatting

--- commands/Jetpack_Site_Connection_Triage.php
+++ commands/Jetpack_Site_Connection_Triage.php
@@ -24,14 +24,24 @@ final class Jetpack_Site_Connection_Triage extends Command {
 	 */
 	private ?array $sites = null;
 
+	/**
+	 * List of known staging domains to check against.
+	 *
+	 * @var array
+	 */
+	private array $staging_domains = array(
+		'mystagingwebsite.com',
+		'jurassic.ninja',
+	);
+
 	protected function configure(): void {
 		$this->setDescription( 'Triages Jetpack connection issues for sites and generates a report.' )
 			->setHelp( 'Use this command to identify and analyze sites with Jetpack connection problems. It will check connection status and generate a CSV report with details about problematic connections. You can either provide a CSV of specific sites to check, or run it without arguments to check all connected Jetpack sites.' );
-	
-		$this->addArgument( 
-			'csv-path', 
-			InputArgument::OPTIONAL, 
-			'A path to a CSV with 2 columns, blog ID and url. If not provided, will check all connected Jetpack sites.' 
+
+		$this->addArgument(
+			'csv-path',
+			InputArgument::OPTIONAL,
+			'A path to a CSV with 2 columns, blog ID and url. If not provided, will check all connected Jetpack sites.'
 		);
 	}
 
@@ -47,17 +57,17 @@ final class Jetpack_Site_Connection_Triage extends Command {
 	 */
 	protected function execute( InputInterface $input, OutputInterface $output ): int {
 		$broken_site_data = array();
-		$csv_path = $input->getArgument( 'csv-path' );
-	
+		$csv_path         = $input->getArgument( 'csv-path' );
+
 		// Get the list of sites to check, either from CSV or from Jetpack
 		if ( $csv_path ) {
 			$output->writeln( "<fg=magenta;options=bold>Reading sites from {$csv_path}...</>" );
-	
+
 			if ( ! file_exists( $csv_path ) ) {
 				$output->writeln( "<fg=red;options=bold>CSV file not found at {$csv_path}.</>" );
 				return Command::FAILURE;
 			}
-	
+
 			// Read the CSV. Column 1: Site ID, Column 2: Site URL
 			$csv = file_get_contents( $csv_path );
 			$csv = explode( "\n", $csv );
@@ -73,16 +83,16 @@ final class Jetpack_Site_Connection_Triage extends Command {
 				}
 			}
 		} else {
-			$output->writeln( "<fg=magenta;options=bold>Checking all Jetpack sites with connection issues...</>" );
-			$output->writeln( "<comment>This initial check may take a few minutes, please be patient...</comment>" );
+			$output->writeln( '<fg=magenta;options=bold>Checking all Jetpack sites with connection issues...</>' );
+			$output->writeln( '<comment>This initial check may take a few minutes, please be patient...</comment>' );
 
 			// Process sites in chunks to avoid memory issues
 			$sites_to_check = array();
 			foreach ( array_chunk( $this->sites, 100, true ) as $sites_chunk ) {
 				$modules = get_jetpack_site_modules_batch( \array_column( $sites_chunk, 'userblog_id' ), $errors );
-				
+
 				foreach ( $errors as $site_id => $error ) {
-					$site = $this->sites[$site_id];
+					$site             = $this->sites[ $site_id ];
 					$sites_to_check[] = array(
 						'blog_id' => $site->userblog_id,
 						'url'     => $site->siteurl,
@@ -90,37 +100,47 @@ final class Jetpack_Site_Connection_Triage extends Command {
 				}
 			}
 		}
-	
+
+		// Helper function to check if domain is a staging domain
+		$is_staging_domain = function( $url ) {
+			foreach ( $this->staging_domains as $staging_domain ) {
+				if ( false !== strpos( $url, $staging_domain ) ) {
+					return true;
+				}
+			}
+			return false;
+		};
+
 		// Process each site through our connection check logic
 		foreach ( $sites_to_check as $site ) {
-			$site_id = $site['blog_id'];
-			$site_url = $site['url'];
-			$domain = parse_url( $site_url, PHP_URL_HOST );
+			$site_id          = $site['blog_id'];
+			$site_url         = $site['url'];
+			$domain           = parse_url( $site_url, PHP_URL_HOST );
 			$jetpack_endpoint = $site_url . '/wp-json/jetpack/v4';
-			$notes = '';
-			$status_code = '';
-			$new_blog_id = '';
-	
+			$notes            = '';
+			$status_code      = '';
+			$new_blog_id      = '';
+
 			$output->writeln( 'Checking ' . $site_url . '...' );
-	
+
 			//fetch
-			$options = array(
+			$options     = array(
 				'http' => array(
 					'method'        => 'GET',
 					'timeout'       => 60,
 					'ignore_errors' => true,
 				),
 			);
-			$context = stream_context_create( $options );
-			$result = @file_get_contents( $site_url, false, $context );
-			$headers = parse_http_headers( $http_response_header );
+			$context     = stream_context_create( $options );
+			$result      = @file_get_contents( $site_url, false, $context );
+			$headers     = parse_http_headers( $http_response_header );
 			$status_code = $headers['http_code'];
-	
-			if ( 410 === $status_code && strpos( $site_url, 'mystagingwebsite.com' ) !== false ) {
+
+			if ( 410 === $status_code && $is_staging_domain( $site_url ) ) {
 				// Probably deactivated
 				$notes = 'Site is probably deactivated';
 				// Check Pressable?
-			} elseif ( 404 === $status_code && strpos( $site_url, 'mystagingwebsite.com' ) !== false ) {
+			} elseif ( 404 === $status_code && $is_staging_domain( $site_url ) ) {
 				// probably deleted or Jetpack not installed. Next step is to curl the homepage
 				//$notes = $result['body'];
 				$notes = '404, check site';
@@ -130,30 +150,32 @@ final class Jetpack_Site_Connection_Triage extends Command {
 			} elseif ( 500 === $status_code ) {
 				//$notes = $result['body'];
 				$notes = 'Check site, internal server error';
-			} elseif ( 200 === $status_code && strpos( $site_url, 'mystagingwebsite.com' ) === false ) {
+			} elseif ( 200 === $status_code && ! $is_staging_domain( $site_url ) ) {
 				$notes = 'Site either moved hosts or has a new JP connection. Check DARC and NA: https://mc.a8c.com/tools/reportcard/domain/?domain=' . $domain . ' and https://wordpress.com/wp-admin/network/sites.php?s=' . $site_url;
-			} elseif ( 200 === $status_code && strpos( $site_url, 'mystagingwebsite.com' ) !== false ) {
+			} elseif ( 200 === $status_code && $is_staging_domain( $site_url ) ) {
 				// Check WPCOM API for site information
 				$wpcom_api_url = 'https://public-api.wordpress.com/rest/v1.1/sites/' . rawurlencode( $domain );
-				$wpcom_result = @file_get_contents( $wpcom_api_url, false, $context );
-				$wpcom_status = $http_response_header ? parse_http_headers( $http_response_header )['http_code'] : '';
-	
+				$wpcom_result  = @file_get_contents( $wpcom_api_url, false, $context );
+				$wpcom_status  = $http_response_header ? parse_http_headers( $http_response_header )['http_code'] : '';
+
 				if ( '400' === $wpcom_status ) {
 					$notes = 'Site is not connected to WordPress.com. If site loads, it may have moved hosts.';
 				} else {
 					$wpcom_data = json_decode( $wpcom_result, true );
-	
+
 					if ( $wpcom_data && isset( $wpcom_data['ID'] ) ) {
 						if ( (int) $wpcom_data['ID'] !== (int) $site_id ) {
-							$notes = 'Site has a new Jetpack connection. Old ID: ' . $site_id . ', New ID: ' . $wpcom_data['ID'];
+							$notes       = 'Site has a new Jetpack connection. Old ID: ' . $site_id . ', New ID: ' . $wpcom_data['ID'];
 							$new_blog_id = $wpcom_data['ID'];
 						} else {
 							$notes = 'Site connection appears valid. Original error may have been intermittent.';
 						}
+					} else {
+						$notes = 'Jetpack is probably disconnected. Log in and reconnect.';
 					}
 				}
 			}
-	
+
 			$broken_site_data[] = array(
 				'site_id'     => $site_id,
 				'site_url'    => $site_url,
@@ -162,21 +184,21 @@ final class Jetpack_Site_Connection_Triage extends Command {
 				'notes'       => $notes,
 			);
 		}
-	
+
 		// Write results to CSV
 		$output->writeln( '<info>Making the CSV...<info>' );
 		$timestamp = gmdate( 'Y-m-d-H-i-s' );
-		$filename = 'jp-connection-' . $timestamp . '.csv';
-		$filepath = getcwd() . DIRECTORY_SEPARATOR . $filename;
-		$fp = fopen( $filepath, 'w' );
-		fputcsv( $fp, array( 'Site ID', 'Site URL', 'Status', 'New Blog ID', 'Notes' ), ",", '"', "\\");
+		$filename  = 'jp-connection-' . $timestamp . '.csv';
+		$filepath  = getcwd() . DIRECTORY_SEPARATOR . $filename;
+		$fp        = fopen( $filepath, 'w' );
+		fputcsv( $fp, array( 'Site ID', 'Site URL', 'Status', 'New Blog ID', 'Notes' ), ',', '"', '\\' );
 		foreach ( $broken_site_data as $fields ) {
-			fputcsv( $fp, $fields, ",", '"', "\\");
+			fputcsv( $fp, $fields, ',', '"', '\\' );
 		}
 		fclose( $fp );
 
 		$output->writeln( sprintf( '<info>Done, CSV saved to: %s</info>', $filepath ) );
-	
+
 		return Command::SUCCESS;
 	}
 
