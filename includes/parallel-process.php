793a5104078403ea642530d0421696535a3c959b
Jared Rethman <jared.rethman@automattic.com>
2025-02-07 08:54:41 -0600

    added ssh_timeout cmd option.
    added `run_callback` to encapsulate repeatable logic

--- includes/parallel-process.php
+++ includes/parallel-process.php
@@ -8,7 +8,6 @@ use Symfony\Component\Process\Process;
 
 class Parallel_Process {
 
-
 	// region FIELDS AND CONSTANTS
 
 	protected OutputInterface $output;
@@ -49,16 +48,23 @@ class Parallel_Process {
 	protected static bool $parallel_extension_loaded;
 
 	/**
+	 * The timeout, in seconds, for the SSH connection process to run.
+	 *
+	 * @var int|null
+	 */
+	protected ?int $ssh_timeout;
+
+	/**
 	 * Callbacks to run after the task is completed.
 	 *
 	 * @var array
 	 */
 	private $callbacks = array(
-		'process_start' => null,
+		'process_start'    => null,
 		'process_complete' => null,
-		'buffer_out' => null,
-		'shell_command' => null,
-		'command_args' => null,
+		'buffer_out'       => null,
+		'shell_command'    => null,
+		'command_args'     => null,
 	);
 
 	// endregion
@@ -66,7 +72,7 @@ class Parallel_Process {
 	// region METHODS
 
 	public function __construct( OutputInterface $output, array $tasks ) {
-		$this->output        = $output;
+		$this->output = $output;
 
 		$this->max_processes = $config['max_processes'] ?? 20;
 		$this->max_threads   = $config['max_threads'] ?? 10;
@@ -93,9 +99,20 @@ class Parallel_Process {
 		return $this;
 	}
 
+	protected function run_callback( string $name, bool $return, ...$args ): mixed {
+		if ( is_callable( $this->callbacks[ $name ] ) ) {
+			if ( $return ) {
+				return ( $this->callbacks[ $name ] )( ...$args );
+			}
+			( $this->callbacks[ $name ] )( ...$args );
+		}
+		return null;
+	}
+
 	public function configure( array $config ): self {
 		$this->max_threads   = $config['max_threads'] ?? 10;
 		$this->max_processes = $config['max_processes'] ?? 20;
+		$this->ssh_timeout   = $config['ssh_timeout'] ?? 120;
 		return $this;
 	}
 
@@ -104,9 +121,9 @@ class Parallel_Process {
 		$start_time = microtime( true );
 
 		$task_failures = array();
-		$processes    = array();
-		$completed    = 0;
-		$results      = array();
+		$processes     = array();
+		$completed     = 0;
+		$results       = array();
 
 		foreach ( $this->tasks as $id => $task ) {
 			if ( ! is_callable( $this->callbacks['command_args'] ) ) {
@@ -121,15 +138,7 @@ class Parallel_Process {
 			$process          = $this->create_ssh_worker_process( ( $this->callbacks['command_args'] )( $id ) );
 			$processes[ $id ] = $process;
 
-			// $this->output->writeln(
-			// 	sprintf(
-			// 		'Started process %s (%s) - Active: %d',
-			// 		$id,
-			// 		$process->getPid(),
-			// 		count( $processes )
-			// 	)
-			// );
-			($this->callbacks['process_start'])( $id, $process->getPid(), count( $processes ) );
+			$this->run_callback( 'process_start', false, $id, $process->getPid(), count( $processes ) );
 
 			$current_processes = count( $processes );
 			while ( $current_processes >= $this->max_processes ) {
@@ -165,34 +174,39 @@ class Parallel_Process {
 	}
 
 	private function create_ssh_worker_process( string $args ): Process {
-		$shell_command = ($this->callbacks['shell_command'])( $args );
-		// $this->output->writeln( "Shell command: $shell_command" );
+		$shell_command = $this->run_callback( 'shell_command', true, $args );
+		if ( ! $shell_command ) {
+			$this->output->writeln(
+				sprintf(
+					'<error>Shell command empty.</error>',
+				)
+			);
+			exit( 1 );
+		}
 		$process = Process::fromShellCommandline(
 			sprintf(
-				'php %s ssh-worker %s --quiet',
-				realpath( __DIR__ . '/../team51-cli.php' ),
-				$args . ' --shell-command="' . $shell_command . '" --quiet',
+				'php %s ssh-worker %s %s %s --quiet',
+				\TEAM51_CLI_FILE,
+				$args,
+				"--shell-command=\"{$shell_command}\"",
+				$this->ssh_timeout ? "--timeout={$this->ssh_timeout}" : ''
 			)
 		);
-		$process->setTimeout( 60 );
+		$process->setTimeout( null ); // Disable timeout, it's handle in the SSH worker.
 		$process->start(
 			function ( $type, $buffer ) {
 				$this->filter_output( $buffer );
-				// if ( Process::ERR === $type ) {
-				// 	$this->filter_output( "<error>Buffer: ERROR: $buffer</error>" );
-				// } else {
-				// }
 			}
 		);
 		return $process;
 	}
 
 	private function filter_output( string $buffer, bool $write_to_output = false ): void {
-		if ( ! $write_to_output && is_callable( $this->callbacks['buffer_out'] ) ) {
-			($this->callbacks['buffer_out'])( $buffer );
-			return;
+		if ( $write_to_output ) {
+			$this->output->writeln( $buffer );
+		} else {
+			$this->run_callback( 'buffer_out', false, $buffer );
 		}
-		$this->output->writeln( $buffer );
 	}
 
 	private function handle_completed_processes( array &$processes, array &$results, array &$task_failures, int &$completed ): void {
@@ -200,29 +214,24 @@ class Parallel_Process {
 			if ( ! $process->isRunning() ) {
 				$process_output = $process->getOutput() ?: $process->getErrorOutput();
 				$result         = \json_decode( $process_output, true );
-
+				$result         = $this->run_callback( 'parse_result', true, $result );
 				if ( $result && ! isset( $result['error'] ) ) {
 					$results[ $index ] = $result;
 				} else {
-					$task_failures[ $index ] = $this->handle_process_result( $result, $index );
+					$error_message           = $result['error'] ?? 'SSH connection failed';
+					$error_details           = $result['details'] ?? 'No details available';
+					$task_failures[ $index ] = (object) array(
+						'error'  => $error_message,
+						'id'     => $result['id'] ?? $index,
+						'errors' => array( $error_message . ': ' . $error_details ),
+					);
 				}
 
 				unset( $processes[ $index ] );
 				++$completed;
-				($this->callbacks['process_complete'])( $index, $completed, $this->task_count, $result );
+				$this->run_callback( 'process_complete', false, $index, $completed, $this->task_count, $result );
 			}
 		}
-	}
-
-	private function handle_process_result( $result, $index ) {
-		$error_message = $result['error'] ?? 'SSH connection failed';
-		$error_details = $result['details'] ?? 'No details available';
-
-		return (object) array(
-			'error'  => $error_message,
-			'id'     => $result['id'] ?? $index,
-			'errors' => array( $error_message . ': ' . $error_details ),
-		);
 	}
 
 	// endregion
